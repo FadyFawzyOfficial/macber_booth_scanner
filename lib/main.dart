3bb0ac33b3b3f927c44185bc0ebf4996596abf8b
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-11-23 10:20:41 +0200

    Use MultipleProvider Widget in main.dart file to get Multi Providers data

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/qr_scanner_page.dart';
+import 'providers/sales_man_provider.dart';
+import 'providers/visitor_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => VisitorProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => SalesManProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: QRScannerPage(),
       ),
-      home: QRScannerPage(),
     );
   }
 }
